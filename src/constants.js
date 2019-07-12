66f050e5b66c39971eee1a3c8ebb11449fb25a95
pricky <ricardo.pereira@hotmail.ch>
2019-07-12 13:52:48 +0200

    New territory added

--- src/constants.js
+++ src/constants.js
@@ -28,6 +28,11 @@ export const TERRITORIES = [
     id: 'tundra',
     name: 'Tundra',
     superpower: 'Let it snow!'
+  },
+  {
+    id: 'portugal',
+    name: 'Portugal',
+    superpower: 'Let it love!'
   }
 ]
 
@@ -52,5 +57,9 @@ export const OTHER_KINGDOMS = [
   {
       app: 'https://thirsty-jang-0c0a17.netlify.com',
       ruler: 'ma1222042.id.blockstack'
+  },
+  {
+    app: 'https://animalkingdoms.netlify.com',
+    ruler: 'moxiegirl.id.blockstack'
   }
 ]
